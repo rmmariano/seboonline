c5e88987b5b0f7bb8f2c7c60319380184f2cfbc2
Rodrigo Mariano <rodmariano13@gmail.com>
2015-08-05 13:46:49 -0300

    refs#2 adicionado smartgrid no meus itens

--- controllers/account.py
+++ controllers/account.py
@@ -1,9 +1,4 @@
 @auth.requires_login() 
-def info():
-	response.flash = T("Welcome to info!")
-	return dict(message=T('Hello World'))
-
-@auth.requires_login() 
 def myitems():
-	response.flash = T("Welcome to web2py!")
-	return dict(message=T('Hello World'))
+	grid = SQLFORM.smartgrid(db.item,linked_tables=['title'])
+	return dict(grid=grid)
