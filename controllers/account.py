aac10d9f28217479742edfecca278b2412d7491b
Rodrigo Mariano <rodmariano13@gmail.com>
2015-08-05 23:47:03 -0300

    refs#2 problema com link da imagem arrumado

--- controllers/account.py
+++ controllers/account.py
@@ -10,11 +10,53 @@ def users():
 def myitems():
 	grid = SQLFORM.smartgrid(db.item,linked_tables=['title'])
 
-	'''
-	if requires_membership(auth.user_id,'administrator'):
-	    print 'passou o/'
-	else:
-	    print 'nao passou :/'
-	'''
+	#TODO: colocar um where na condicao, aparecer os itens somente do usuario X
 
 	return dict(grid=grid)
+
+######################################################
+def user():
+    """
+    exposes:
+    http://..../[app]/default/user/login
+    http://..../[app]/default/user/logout
+    http://..../[app]/default/user/register
+    http://..../[app]/default/user/profile
+    http://..../[app]/default/user/retrieve_password
+    http://..../[app]/default/user/change_password
+    http://..../[app]/default/user/manage_users (requires membership in
+    use @auth.requires_login()
+        @auth.requires_membership('group name')
+        @auth.requires_permission('read','table name',record_id)
+    to decorate functions that need access control
+    """
+    return dict(form=auth())
+
+@cache.action()
+def download():
+    """
+    allows downloading of uploaded files
+    http://..../[app]/default/download/[filename]
+    """
+    return response.download(request, db)
+
+def call():
+    """
+    exposes services. for example:
+    http://..../[app]/default/call/jsonrpc
+    decorate with @services.jsonrpc the functions to expose
+    supports xml, json, xmlrpc, jsonrpc, amfrpc, rss, csv
+    """
+    return service()
+
+@auth.requires_login() 
+def api():
+    """
+    this is example of API with access control
+    WEB2PY provides Hypermedia API (Collection+JSON) Experimental
+    """
+    from gluon.contrib.hypermedia import Collection
+    rules = {
+        '<tablename>': {'GET':{},'POST':{},'PUT':{},'DELETE':{}},
+        }
+    return Collection(db).process(request,response,rules)
