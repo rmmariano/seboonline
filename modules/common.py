1d37cae5c393622c9cca8ff3fb2d199b6796add0
Rodrigo Mariano <rodmariano13@gmail.com>
2015-08-10 20:44:58 -0300

    refs#3 arrumado bug do Minha Conta aparecer para todos

--- modules/common.py
+++ modules/common.py
@@ -3,6 +3,9 @@
 from gluon import current
 
 def requires_membership(auth_user_id,group_role):
+	
+	#TODO:  arrumar essa função pegando o grupo da sessao, sem fazer pesquisa no db
+
 	db = current.db #referência de memória do db corrente
 	group=db(db.auth_group.role==group_role).select()
 	isempty=db((db.auth_membership.user_id==auth_user_id)&(db.auth_membership.group_id==group[0].id)).isempty()
